ce894c4df53b22a947df759dc29a3c41a312ad8b
sivaram143 <sivaram.nyayapati@gmail.com>
2017-06-16 19:48:26 +0530

    updating lex

--- lex.js
+++ lex.js
@@ -3,11 +3,9 @@ AWS.config.update({region: 'us-east-1'});
 
 var lexruntime = new AWS.LexRuntime({apiVersion: '2016-11-28'});
 
-console.log("lex..."+JSON.stringify(lexruntime));
-
 var params = {
   botAlias: '$LATEST', /* required, has to be '$LATEST' */
-  botName: 'ConferenceRoom', /* required, the name of you bot */
+  botName: 'BookConferenceRoom', /* required, the name of you bot */
   inputText: 'Hi ', /* required, your text */
   userId: 'sivaram', /* required, arbitrary identifier */
   sessionAttributes: {
@@ -16,6 +14,11 @@ var params = {
 };
 
 lexruntime.postText(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
+  if (err) {
+    console.log(err, err.stack); 
+  }
+  else{
+  	console.log(data);   
+  	// successful response
+  }     
 });
